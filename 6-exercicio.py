709895cb558831dd69c2cf6859aa8d98dc6d4206
PEDRO033 <141421705+PEDRO033@users.noreply.github.com>
2026-01-13 11:59:38 -0300

    Dia de exercício
     Antecessor e Sucessor de um número, Média de 4 notas.  O outro eu fiz Por conta. Para não ficar limitado a uma única divisão, podendo escolher por quanto será a divisão!

---
+++ 6-exercicio.py
@@ -0,0 +1,46 @@
+# Antecessor e Sucessor de um número
+# Escreva um programa em Python que leia um número 
+# e represente o número antecessor e sucessor desse número
+# que foi lido, utilizando operadores de atribuição.
+
+# Média de 4 notas
+# Escreva um programa em Python que leia quatro números e calcule a média entre esses números
+
+# Antecessor e Sucessor
+print("=====Antecessor e Sucessor=====\n")
+
+num11 = int(input("Digite o Seu Número: \n")) 
+num22 = int(input("Digite Seu Sgundo Número: \n"))
+s = num11 + 1
+a = num11 - 1
+s2 = num22 + 1
+a2 = num22 - 1
+print(f"\n***O Sucessor de {num11} é {s} e o Antecessor de {num11} é {a}***")
+print(f"\n***O Sucessor de {num22} é {s2} e o Antecessor de {num22} é {a2}***\n")
+# Nesse minha principal dificuldade foi acertar os operadores
+# Ou Foi a Máquina ou o Professor Fez Errado
+
+#Média de 4 números
+print("=====Média de 4 números=====\n")
+
+nu1 = int(input("Digite seu Número 1 : \n"))
+nu2 = int(input("Digite seu Número 2 : \n"))
+nu3 = int(input("Digite seu Número 3 : \n"))
+nu4 = int(input("Digite seu Número 4 : \n"))
+med = int(nu1 + nu2 + nu3 + nu4)
+merr = med / 4 
+print(f"\n A Média dos 4 Número é ***{merr}***\n")
+#Aqui Minha Difilcudade foi não ter feito a variável pra dividir 
+
+# Divisão pelo Número Escolhido
+print("=====Divisão pelo Número Escolhido=====\n")
+
+medr = int(input("Digite por quanto Deseja Dividir: \n"))
+nu10 = int(input("Digite seu Número 1 : \n"))
+nu20 = int(input("Digite seu Número 2 : \n"))
+nu30 = int(input("Digite seu Número 3 : \n"))
+nu40 = int(input("Digite seu Número 4 : \n"))
+mer  =  int( nu10 + nu20 + nu30 + nu40 )
+div  = mer / medr
+print(f"\n O Resultado da Sua Divisão é: ***{div}***\n")
+# Aqui minha Dificuldade foi a Interpretação
